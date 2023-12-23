bb09e2fd870fad8b508e01e4cc571d0c553380e0
IceMupppet <sakibdalal73@gmail.com>
2023-12-23 18:37:33 +0530

    operations dictionaries

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,14 +1,17 @@
-i = {1: 10, 2: 20, 3: 30, 4: 40}
-print(i)
-# Basic Operations
-# add
-i[5] = 50
-print(i)
-# modify
-i[5] = 90
-print(i)
-# delete
-del (i[5])
+s1 = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50}
+s2 = {6: 60, 7: 70, 8: 80, 9: 90, 10: 100}
+# doesnt work print(s1 + s2)
+# doesnt work s3 = {1: 10} + s2
 
-print(sorted(i))
-del (i)
+# cloning
+s3 = s1
+print(s3)
+
+print(1 in s1)
+print(10 in s1.values())
+print(s1 is s2)
+print(s1 is s3)
+
+# aliasing is normally adding
+s1[0] = 10
+print(s1)
