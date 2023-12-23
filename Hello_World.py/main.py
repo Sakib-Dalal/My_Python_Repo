9ecdab55c20c39b10755ef24aab7a465810ddd50
IceMupppet <sakibdalal73@gmail.com>
2023-12-23 18:56:49 +0530

    dictionary methods

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,6 +1,14 @@
 s = {1: 10, 2: 20, 3: 30, 4: 40}
-x = {}
-for i, j in reversed(s.items()):
-    print(i, j)
-    x[i] = j
-print(x)
+p = {5: 50, 6: 60, 7: 70, 8: 80}
+
+print(s.get(3, "Absent"))
+print(s.get(5, "absent"))
+print(s)
+print(s[2])
+
+print(s.pop(1))
+print(s)
+print(s.popitem())
+print(s)
+print(s.update(p))
+print(s)
