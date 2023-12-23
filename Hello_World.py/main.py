a4070c1fc5762533f7942d575fc4aa2aec6e54a6
IceMupppet <sakibdalal73@gmail.com>
2023-12-23 15:46:25 +0530

    Intro to Dictionaries

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,9 +1,6 @@
-l = [10, 20, 30, 40, 50]
-t = ("Sundeep", 25, 79.58)
-s = "set theory"
-s1 = set(l)
-s2 = set(t)
-s3 = set(s)
-print(s1)
-print(s2)
-print(s3)
+# Dictionaries
+# {key:value}
+d1 = {}
+d2 = {1: "Amole", 2: "Anil", 3: "Ravi"}
+print(d1, d2)
+print(type(d1), type(d2))
