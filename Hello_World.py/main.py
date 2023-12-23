216ee3106cc3a84fb3cd1bd3317938d159d3c6ff
IceMupppet <sakibdalal73@gmail.com>
2023-12-23 18:47:50 +0530

    reverse in dictionaries

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,21 +1,6 @@
 s = {1: 10, 2: 20, 3: 30, 4: 40}
-print(len(s))
-print(len(s.items()))
-print(len(s.keys()))
-print(len(s.values()))
-
-print(max(s))
-print(max(s.keys()))
-print(max(s.values()))
-print(sum(s))
-print(sum(s.keys()))
-print(sum(s.values()))
-print(any(s))
-print(all(s))
-
-x = {"A01": "Sakib", "A02": "Raj", "A03": "ABC"}
-print(len(x))
-print(max(x))
-print(min(x))
-print(max(x.values()))
-print(min(x.values()))
+x = {}
+for i, j in reversed(s.items()):
+    print(i, j)
+    x[i] = j
+print(x)
