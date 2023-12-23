42f4e290336b7cb7e874ec3f98226e091f31bcd7
IceMupppet <sakibdalal73@gmail.com>
2023-12-23 16:10:14 +0530

    Dictionaries for loop

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,6 +1,23 @@
-# Dictionaries
-# {key:value}
-d1 = {}
-d2 = {1: "Amole", 2: "Anil", 3: "Ravi"}
-print(d1, d2)
-print(type(d1), type(d2))
+i = {1: 30, 2: 40, 3: 70, 4: 20}
+print(i)
+print(i.items())
+print(i.keys())
+print(i.values())
+
+for x in i:
+    print(x)
+for x in i.keys():
+    print(x)
+for x in i.values():
+    print(x)
+for x, y in i.items():
+    print(x)
+
+for x in i.items():
+    print(x)
+    print(type(x))
+
+for x in i.items():
+    print(x[0])
+for x in i.items():
+    print(x[1])
