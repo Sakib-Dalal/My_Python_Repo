f515484ae07973a040a2fbc54314dcc58e7e44c2
IceMupppet <sakibdalal73@gmail.com>
2023-12-23 19:21:08 +0530

    dictionary nesting and accessing

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,14 +1,13 @@
-s = {1: 10, 2: 20, 3: 30, 4: 40}
-p = {5: 50, 6: 60, 7: 70, 8: 80}
-
-print(s.get(3, "Absent"))
-print(s.get(5, "absent"))
+s = {
+    "a": {1: 10, 2: 20},
+    "b": {3: 30, 4: 40},
+    "c": {5: 50, 6: 60}
+}
 print(s)
-print(s[2])
-
-print(s.pop(1))
-print(s)
-print(s.popitem())
-print(s)
-print(s.update(p))
-print(s)
+for i, j in s.items():
+    print(i, j)
+for i, j in s["a"].items():
+    print(i, j)
+for i, j in s["c"].items():
+    print(i, j)
+    
