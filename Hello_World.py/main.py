139ff9141bf8805dfb4148260e87261588459396
IceMupppet <sakibdalal73@gmail.com>
2023-12-26 18:34:59 +0530

    List Comprehansion multiple for's

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,23 +1,12 @@
-# List Comprehensions
-import random
-a = [random.randint(0, 100) for n in range(20)]
+arr = [[1, 2, 3, 4], [5, 6, 7, 8], [10, 11, 12, 13]]
+b = [i for j in arr for i in j]
+print(b)
+
+# OR
+a = [*arr[0], *arr[1], *arr[2]]
 print(a)
 
-# square and cube root from 0 to 10
-a = [(x, x**2, x**3) for x in range(10)]
-print(a)
+# print 0 to 4 10 times
+m = [i for j in range(10) for i in range(5)]
+print(m)
 
-# normal example
-a = [x for x in range(10)]
-print(a)
-
-a = [int(x) for x in ["10", "20", "30", "40", "50"]]
-print(a)
-
-# include numbers in list if it is completely divisible by 2
-a = [x for x in range(10, 30) if x % 2 == 0]
-print(a)
-
-# include n from list "a" if n is gater than 20
-a = [n for n in a if n > 20]
-print(a)
