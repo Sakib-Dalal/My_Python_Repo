10b3f9ede4e5fef0e5149b10500c133bb5acbd47
IceMupppet <sakibdalal73@gmail.com>
2023-12-26 19:43:17 +0530

    all about list comprehension

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -6,3 +6,10 @@ for i in range(2, 51):
     if i % 2 == 0 and i % 4 == 0:
         l.append(i)
 print(l)
+
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+a = [i for j in mat for i in j]
+print(a)
+
+a = [*mat[0], *mat[1], *mat[2]]
+print(a)
