6ceec8218c80db106c08617c0c778ea558294197
IceMupppet <sakibdalal73@gmail.com>
2023-12-26 19:21:07 +0530

    Comprehansion Dictionaries

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,2 +1,15 @@
-a = [(i, j, k) for i in [1, 2, 3] for j in [1, 2, 3] for k in [1, 2, 3] if i != j and j != k and k != i]
-print(a)
+d = {"a": 1, "b": 2, "c": 3, "d": 4}
+
+d1 = {i: j**3 for (i, j) in d.items()}
+print(d1)
+
+d2 = {i: j**3 for (i, j) in d.items() if j > 3}
+print(d2)
+
+g = {1: 2, 3: 4}
+print(g[3])
+
+d3 = {i: ("Even" if j % 2 == 0 else "Odd") for (i, j) in d.items()}
+print(d3)
+
+
