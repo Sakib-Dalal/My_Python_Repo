ed694e9e5056e2c80afe9af664d6d451c5a06e53
IceMupppet <sakibdalal73@gmail.com>
2023-12-26 16:27:43 +0530

    List Comprehansion

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,6 +1,4 @@
-student = {1: "Ashok", 2: "Raj", 3: "Govind"}
-
-roll_no = int(input("Enter a number: "))
-
-name = student.get(roll_no, "Student")
-print(f"Congratulation {name}")
+# List Comprehensions
+import random
+a = [random.randint(0, 100) for n in range(20)]
+print(a)
