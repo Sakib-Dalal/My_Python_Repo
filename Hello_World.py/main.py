46aa3207211caa3f61b0bbf462bee3cb9f29f49a
IceMupppet <sakibdalal73@gmail.com>
2023-12-26 18:45:29 +0530

    List Comprehansion multiple for's and if

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,18 +1,2 @@
-arr = [[1, 2, 3, 4], [5, 6, 7, 8], [10, 11, 12, 13]]
-b = [i for j in arr for i in j]
-print(b)
-
-# OR
-a = [*arr[0], *arr[1], *arr[2]]
+a = [(i, j, k) for i in [1, 2, 3] for j in [1, 2, 3] for k in [1, 2, 3] if i != j and j != k and k != i]
 print(a)
-
-# print 0 to 4 10 times
-m = [i for j in range(10) for i in range(5)]
-print(m)
-
-d = [a + b for a in [1, 2, 3] for b in [4, 5, 6]]
-print(d)
-
-d = [[a + b for a in [1, 2, 3]] for b in [4, 5, 6]]
-print(d)
-
