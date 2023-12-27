78a7fdcaa8d70158259cd04bcb7b232eb9390abb
IceMupppet <sakibdalal73@gmail.com>
2023-12-27 15:26:55 +0530

    using default value as argument

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,15 +1,6 @@
-def print_it(i, j, *args, x, y, **kwargs):
-    print()
-    print(i, j, end=" ")
-    for v in args:
-        print(v, end=" ")
-    print(x, y, end=" ")
-    for k, v in kwargs.items():
-        print(k, v, end=" ")
+def fun(a, b=3.14):
+    print(2 * b * a)
 
 
-print_it(10, 20, x=30, y=40)
-
-print_it(10, 20, 100, 200, x=30, y=40)
-
-print_it(10, 20, 100, 200, a=15, b=30, c=45, x=30, y=40)
+fun(4)
+fun(4, 3)
