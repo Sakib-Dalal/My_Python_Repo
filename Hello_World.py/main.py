586eb6f7e6de266aaac8a800803d4b597045c739
IceMupppet <sakibdalal73@gmail.com>
2023-12-27 11:52:40 +0530

    function nested function

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,15 +1,12 @@
-a = [n for n in range(2, 51) if n % 2 == 0 and n % 4 == 0]
-print(a)
+def fun1():
+    print("Hello World 01")
 
-l = []
-for i in range(2, 51):
-    if i % 2 == 0 and i % 4 == 0:
-        l.append(i)
-print(l)
+    def fun2():
+        print("Hello World 02")
 
-mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-a = [i for j in mat for i in j]
-print(a)
+    fun2()
 
-a = [*mat[0], *mat[1], *mat[2]]
-print(a)
+
+# calling the function
+fun1()
+
