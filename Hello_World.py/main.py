be9781889cee74eb8121be013b2330711df66e6c
IceMupppet <sakibdalal73@gmail.com>
2023-12-27 12:25:12 +0530

    function return list, tuple, set, dictionaries

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,6 +1,34 @@
-def fun():
-    return
+def fun(x, y, z):
+    return x + y, y + z, x / y, round(y / z, 2)
 
 
-s = fun()
+s = fun(1, 2, 3)
 print(s)
+print(type(s))
+
+
+def fun2(x, y, z):
+    return [x, y, z]
+
+
+s = fun2("Hello", "World", "!")
+print(s)
+print(type(s))
+
+
+def fun3(x, y, z):
+    return {x, y, x}
+
+
+s = fun3("Hello", "World", "!")
+print(s)
+print(type(s))
+
+
+def fun4(x, y, z, a):
+    return {x: y, z: a}
+
+
+s = fun4("A", 1, "B", 2)
+print(s)
+print(type(s))
