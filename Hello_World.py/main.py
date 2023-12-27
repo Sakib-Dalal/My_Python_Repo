715154e78e24c8468e7a97c3843f84b2802268e4
IceMupppet <sakibdalal73@gmail.com>
2023-12-27 16:15:15 +0530

    sum and product in function by taking input by user as parameter

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,47 +1,12 @@
-# unpacking list/tuple/set argument
-
-def fun(a, b, c, d):
-    print(a, b, c, d)
-
-
-lst = [1, 2, 3, 4]
-tpl = (1, 2, 3, 4)
-s = {1, 2, 3, 4}
-
-fun(*lst)
-fun(*tpl)
-fun(*s)
+def cal_sum_prod(x, y, z):
+    ss = x + y + z
+    pp = x * y * z
+    return ss, pp
 
 
-# using *args
-def fun2(*args):
-    print()
-    for v in args:
-        print(v, end=" ")
+a = int(input("Enter a num: "))
+b = int(input("Enter b num: "))
+c = int(input("Enter c num: "))
 
-
-fun2(*lst)
-fun2(*tpl)
-fun2(*s)
-print()
-
-
-# for dictionaries using keywords
-def fun3(name, marks):
-    print(name, marks)
-
-
-d = {"name": "Sakib", "marks": 50}
-fun3(*d)
-fun3(**d)
-
-
-# with multiple values
-def fun4(**kwargs):
-    print()
-    for k, v in kwargs.items():
-        print(k, v, end=" ")
-
-
-my_dic = {"1": 10, "2": 20, "3": 30, "4": 40}
-fun4(**my_dic)
+s, p = cal_sum_prod(a, b, c)
+print(s, p)
