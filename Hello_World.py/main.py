a5a24400bcb2701b74484ae7c0efd3a3d1ff0c71
IceMupppet <sakibdalal73@gmail.com>
2023-12-27 15:01:35 +0530

    All types of arguments in one

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,62 +1,15 @@
-# position argument
-
-def fun1(i, j, k):
-    print(i + j)
-    print(k.upper())
-
-
-fun1(13, 3.14, "Hello")
-
-
-# keyword argument
-
-def fun2(i, f, str):
-    print(i, f, str)
-
-
-fun2(12, 13.2, 'Hello')
-fun2(i=12, f=13.2, str='Hello')
-fun2(str='Hello', i=12, f=13.2)
-fun2(f=13.2, str='Hello', i=12)
-
-fun2(12, f=13.2, str='Hello')
-fun2(12, str='Hello', f=13.2)
-
-
-# if number of parameters in a function are unknown
-# variable length position
-def fun3(*args):
+def print_it(i, j, *args, x, y, **kwargs):
     print()
+    print(i, j, end=" ")
     for v in args:
         print(v, end=" ")
+    print(x, y, end=" ")
+    for k, v in kwargs.items():
+        print(k, v, end=" ")
 
 
-fun3(3)
-fun3("Hello", 3, 23, 34.35)
+print_it(10, 20, x=30, y=40)
 
+print_it(10, 20, 100, 200, x=30, y=40)
 
-def fun4(*args):
-    print()
-    for v in args:
-        print(v + (v + 1))
-
-
-fun4(3, 4, 5)
-
-
-# variable length keyword
-# works as a dictionary
-def print_it(**kwargs):
-    print()
-    for key, value in kwargs.items():
-        print(key, value, end=" ")
-
-
-print_it(a=10)
-print_it(a=10, b=3)
-print_it(a=10, b=3, s="Hello")
-
-dic = {"1": "10", "2": "20", "3": "30", "4": 40}
-print_it(**dic)
-
-
+print_it(10, 20, 100, 200, a=15, b=30, c=45, x=30, y=40)
