af3da270942912c94b4f70b5d4246e36eefb109f
Sakib_Dalal <sakibdalal73@gmail.com>
2023-12-30 13:00:38 +0530

    prime number checker using function

--- Hello_World.py/main.py
+++ Hello_World.py/main.py
@@ -1,13 +1,13 @@
-import math
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It is a prime number.")
+    else:
+        print("It is not a prime number.")
 
 
-def paint_cal(height, width, cover):
-    num_of_can = (height * width) / cover
-    print(f"number of can required: {math.ceil(num_of_can)}")
-
-
-test_h = int(input("Enter Height of Wall: "))
-test_w = int(input("Enter Width of Wall: "))
-coverage = 5
-
-paint_cal(height=test_h, width=test_w, cover=coverage)
+num = int(input("Enter a number to check: "))
+prime_checker(num)
